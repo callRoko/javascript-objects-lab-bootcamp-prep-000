d5ae60d95b9fbb0fe67b2a22f9bb75583e75e288
Hazel Donaldson <hazel90.hd@gmail.com>
2019-04-09 14:38:46 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,26 @@
+var recipes = { };
+
+function updateObjectWithKeyAndValue(object, key, value){
+ object[key] = value
+ return object
+}
+
+function updateObjectWithAndValue(object, key, value){
+  object.assign({}, obj, { [key]: value })
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  
+}
+
+function deleteFromObjectByKey(object, key){
+  
+}
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  
+}
+
+function destructivelyDeleteFRomObjectByKey(object, key){
+  
+}
